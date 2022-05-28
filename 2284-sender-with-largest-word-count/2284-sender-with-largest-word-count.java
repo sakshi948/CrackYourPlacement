dd06ea31922e0cf7c2e6b2e1ec45a309cfe8e31c
sakshi948 <54503255+sakshi948@users.noreply.github.com>
2022-05-28 23:14:31 +0530

    Time: 47 ms (22.22%), Space: 72.3 MB (11.11%) - LeetHub

--- 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.java
+++ 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.java
@@ -1,45 +1,48 @@
 class Solution {
-    public static int num(String str) {
+    public static int count(String str)
+    {
         if(str.trim().isEmpty()) {
             return 0;
         }
         int wordCount = 1;
         int len = str.length();
         for(int i = 0; i < len-1; i++) {
-           if(str.charAt(i) == ' ') { 
+           if(str.charAt(i) == ' ' && str.charAt(i+1) != ' ') { 
                 wordCount++;
            }
         }
         return wordCount;
     }
     public String largestWordCount(String[] messages, String[] senders) {
-        HashMap<String,Integer> hm  = new HashMap<>();
-        int n = senders.length;
-        for(int i = 0; i<n;i++){
+        HashMap<String,Integer> hm=new HashMap<>();
+        for(int i=0;i<senders.length;i++)
+        {
             if(hm.containsKey(senders[i]))
-                hm.put(senders[i],hm.get(senders[i])+num(messages[i]));
-            else
-                hm.put(senders[i],num(messages[i]));
-                
-            
-            
-        }
-        int ans = Integer.MIN_VALUE;
-        String s = "";
-        for(var v : hm.keySet()){
-            if(hm.get(v) > ans){
-                ans = hm.get(v);
-                s = v;
-            }
-            else if(hm.get(v) == ans && v.compareTo(s) > 0)
             {
-                s = v;
+                hm.put(senders[i],hm.get(senders[i])+count(messages[i]));
+            }
+            else{
+                hm.put(senders[i],count(messages[i]));
             }
         }
-        
-        return s;
-        
-        
-        
+        int ans=Integer.MIN_VALUE;
+        String s=" ";
+        for(var v:hm.keySet())
+        {
+            if(hm.get(v)>ans)
+            {
+                ans=hm.get(v);
+                s=v;
+            }
+            else if(hm.get(v)==ans)
+            {
+                if(v.compareTo(s)>0)
+                {
+                    s=v;
+                    ans=hm.get(v);
+                }
+            }
+        }
+            return s;
     }
 }
