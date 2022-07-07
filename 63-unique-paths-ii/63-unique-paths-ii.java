36a725195c8d6c5af0a24d3553839fb0c909a62e
sakshi948 <54503255+sakshi948@users.noreply.github.com>
2022-07-07 18:14:48 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (89.23%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,19 +1,22 @@
 class Solution {
-    public int f(int [][]arr,int m,int n,int dp[][])
-    {
-        if(m==0&&n==0&&arr[m][n]==0){return 1;}
-        if(m<0||n<0){return 0;}
-        if(arr[m][n] == 1){return 0;}
-        if(dp[m][n]!=-1){return dp[m][n];}
-        int up = f(arr,m-1,n,dp);
-        int left = f(arr,m,n-1,dp);
-        return dp[m][n] = up+left;
-    }
     public int uniquePathsWithObstacles(int[][]arr) {
         int m=arr.length;
         int n=arr[0].length;
         int dp[][] = new int[m][n];
-        for(int[] row : dp){Arrays.fill(row,-1);}
-        return f(arr,m-1,n-1,dp);
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0&&j==0&&arr[0][0]==0){dp[0][0]=1;}
+                else if(arr[i][j]==1){dp[i][j]=0;}
+                else{
+                    int up=0,left=0;
+                    if(i>0){up = dp[i-1][j];}
+                    if(j>0){left = dp[i][j-1];}
+                    dp[i][j] = up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
