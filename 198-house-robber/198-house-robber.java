74cba0211d67dbb9c55e4f5ecec26c81f2063d85
sakshi948 <54503255+sakshi948@users.noreply.github.com>
2022-07-08 12:26:19 +0530

    Time: 1 ms (20.51%), Space: 41.7 MB (30.78%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,18 +1,18 @@
 class Solution {
-    public int robbery(int nums[],int n,int dp[])
+    int f(int[] nums,int n,int[] dp)
     {
+        if(n==0){return nums[0];}
         if(n<0){return 0;}
-        if(n==0){return dp[n] = nums[0];}
+        int taken=0,notTaken=0;
         if(dp[n]!=-1){return dp[n];}
-        int taken = nums[n]+robbery(nums,n-2,dp);
-        int nottaken = robbery(nums,n-1,dp);
-        return dp[n]=Math.max(taken,nottaken);
+        taken = nums[n] + f(nums,n-2,dp);
+        notTaken = f(nums,n-1,dp);
+        return dp[n] = Math.max(taken,notTaken);
     }
     public int rob(int[] nums) {
         int n=nums.length;
         int dp[] = new int[n];
         Arrays.fill(dp,-1);
-        return robbery(nums,n-1,dp);
+        return f(nums,n-1,dp);
     }
 }
-//tc O(2^n)
