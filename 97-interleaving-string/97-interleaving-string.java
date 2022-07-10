31068b266e8f0a4ce436700b00ef3917ae9b37e3
sakshi948 <54503255+sakshi948@users.noreply.github.com>
2022-07-10 16:08:16 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (90.13%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -1,35 +1,38 @@
 class Solution {
+    private boolean[][] invalid;
+    private char[] c1;
+    private char[] c2;
+    private char[] c3;
+    
     public boolean isInterleave(String s1, String s2, String s3) {
-	if(s1.length() + s2.length() != s3.length())
-		return false;
-
-	Boolean[][] memo = new Boolean[s1.length() + 1][s2.length() + 1];
-
-	return helper(s1, s2, s3, 0, 0, memo);
-}
-
-public boolean helper(String s1, String s2, String s3, int i, int j, Boolean[][] memo) {
-	if(i+j == s3.length())
-		return true;
-
-	if(memo[i][j] != null)
-		return memo[i][j];
-
-	 if(i < s1.length() && s3.charAt(i+j) == s1.charAt(i)) {
-		boolean flag = helper(s1, s2, s3, i+1, j, memo);
-		memo[i][j] = flag;
-		if(flag) 
-			return true;
-	}
-
-	if(j < s2.length() && s3.charAt(i+j) == s2.charAt(j)) {
-		boolean flag = helper(s1, s2, s3, i, j+1, memo);
-		memo[i][j] = flag;
-		if(flag) 
-			return true;
-	}
-
-	memo[i][j] = false;
-	return false;
-}
+        c1 = s1.toCharArray();
+        c2 = s2.toCharArray();
+        c3 = s3.toCharArray();
+        
+        int m = s1.length(),n=s2.length();
+        
+        if(m+n != s3.length())
+            return false;
+        
+        invalid = new boolean[m+1][n+1];
+        
+        return dfs(0,0,0);
+    }
+    
+    public boolean dfs(int i, int j, int k){
+        if(invalid[i][j])
+            return false;
+        
+        if(k == c3.length)
+            return true;
+        
+        boolean valid = 
+            i<c1.length && c1[i] == c3[k] && dfs(i+1,j,k+1) || 
+            j<c2.length && c2[j] == c3[k] && dfs(i,j+1,k+1);
+        
+        if(!valid)
+            invalid[i][j] = true;
+        
+        return valid;
+    }
 }
