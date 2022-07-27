8167969dfddb8f261226be36ad4370fcfc21f541
sakshi948 <54503255+sakshi948@users.noreply.github.com>
2022-07-27 20:23:47 +0530

    Time: 280 ms (8.58%), Space: 245.5 MB (5.06%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,21 +1,17 @@
 class Solution {
-    public int fn(int[] arr,int ind,int prev,int n,int[][] dp){
-        if(ind==n){return 0;}
-        if(dp[ind][prev+1]!=-1){return dp[ind][prev+1];}
-        dp[ind][prev+1] = fn(arr,ind+1,prev,n,dp);
-        if(prev==-1 || arr[prev]<arr[ind]){   
-            dp[ind][prev+1] = Math.max(dp[ind][prev+1],1+fn(arr,ind+1,ind,n,dp));
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int dp[][] = new int[n+1][n+1];
+        
+        for(int ind=n-1;ind>=0;ind--){
+          for(int prev=ind-1;prev>=-1;prev--){
+              int len = dp[ind+1][prev+1];
+              if(prev==-1||nums[ind]>nums[prev]){
+                  len = Math.max(len,1+dp[ind+1][ind+1]);
+              }
+              dp[ind][prev+1] = len;
+          }  
         }
-        return dp[ind][prev+1];
-    }
-    public int lengthOfLIS(int[] arr) {
-        int n=arr.length;
-        int dp[][] = new int[n][n+1];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=n;j++){
-                dp[i][j] = -1;
-            }
-        }
-        return fn(arr,0,-1,n,dp);
+        return dp[0][0];
     }
 }
