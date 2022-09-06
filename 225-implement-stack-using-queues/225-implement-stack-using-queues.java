9844c26133290400d18399f5c222a980cf30dca9
sakshi948 <54503255+sakshi948@users.noreply.github.com>
2022-09-06 23:16:06 +0530

    Time: 1 ms (34.06%), Space: 41.6 MB (72.96%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.java
@@ -1,36 +1,26 @@
 class MyStack {
     Queue<Integer> q1;
-    Queue<Integer> q2;
     public MyStack() {
         q1 = new LinkedList<>();
-        q2 = new LinkedList<>();
     }
-    
     public void push(int x) {
-        q2.offer(x);
-        while(!q1.isEmpty()){
-            q2.offer(q1.poll());
-           // q1.poll();
-        }
-        
-        while(!q2.isEmpty()){
-            q1.offer(q2.poll());
-        //    q2.poll();
+        q1.offer(x);
+        for(int i=0;i<q1.size()-1;i++){
+            q1.offer(q1.poll());
         }
     }
-    
     public int pop() {
-        if(q1.isEmpty()){return -1;}
+        if(q1.size()==0){return -1;}
         return q1.poll();
     }
     
     public int top() {
-        if(q1.isEmpty()){return -1;}
+        if(q1.size()==0){return -1;}
         return q1.peek();
     }
     
     public boolean empty() {
-        if(q1.size()==0){return true;}
+       if(q1.size()==0){return true;}
         return false;
     }
 }
