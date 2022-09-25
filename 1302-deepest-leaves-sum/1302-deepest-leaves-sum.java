ea3c2c747fbab1b241fac8732c8001a785e577a8
sakshi948 <54503255+sakshi948@users.noreply.github.com>
2022-09-25 10:48:21 +0530

    Time: 4 ms (71.61%), Space: 55.3 MB (73.76%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -13,19 +13,22 @@
  *     }
  * }
  */
-class Solution{
-  public int deepestLeavesSum(TreeNode root) {
-        int res = 0, i;
-        LinkedList<TreeNode> q = new LinkedList<TreeNode>();
-        q.add(root);
-        while (!q.isEmpty()) {
-            for (i = q.size() - 1, res = 0; i >= 0; --i) {
-                TreeNode node = q.poll();
-                res += node.val;
-                if (node.right != null) q.add(node.right);
-                if (node.left  != null) q.add(node.left);
-            }
-        }
-        return res;
+class Solution {
+    public int fn(TreeNode root,int height){
+        if(root==null){return 0;}
+        if(height==1){return root.val;}
+        int left = fn(root.left,height-1);
+        int right = fn(root.right,height-1);
+        return left+right;
+    }
+    public int height(TreeNode root){
+        if(root==null){return 0;}
+        int left = height(root.left);
+        int right = height(root.right);
+        return 1+Math.max(left,right);
+    }
+    public int deepestLeavesSum(TreeNode root) {
+        int h = height(root);
+        return fn(root,h);
     }
 }
