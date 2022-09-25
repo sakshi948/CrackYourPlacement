527d560f9861af71caccac032953e7961f274472
sakshi948 <54503255+sakshi948@users.noreply.github.com>
2022-09-25 10:37:05 +0530

    Time: 5 ms (63.28%), Space: 44.6 MB (95.08%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -13,24 +13,19 @@
  *     }
  * }
  */
-class Solution {
-    public int deepestLeavesSum(TreeNode root) {
-        Queue<TreeNode> qu = new LinkedList<>();
-        qu.offer(root);
-        int sum=0,ans=0;
-        while(!qu.isEmpty()){
-            int level = qu.size();
-            for(int i=0;i<level;i++){
-                TreeNode curr = qu.poll();  
-                sum+=curr.val;
-                if(curr.left!=null){qu.offer(curr.left);}
-                if(curr.right!=null){qu.offer(curr.right);}
+class Solution{
+  public int deepestLeavesSum(TreeNode root) {
+        int res = 0, i;
+        LinkedList<TreeNode> q = new LinkedList<TreeNode>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            for (i = q.size() - 1, res = 0; i >= 0; --i) {
+                TreeNode node = q.poll();
+                res += node.val;
+                if (node.right != null) q.add(node.right);
+                if (node.left  != null) q.add(node.left);
             }
-            ans=sum;
-            System.out.println(ans);
-            sum=0;
         }
-        
-        return ans;
+        return res;
     }
 }
